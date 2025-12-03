a7b95ddd3c97f30f6d00fb1715fde9b4b007c46c
ricard-inho <ricardmarsalicastan@gmail.com>
2025-12-03 10:18:36 +0100

    multi-critic

--- scripts/reinforcement_learning/rsl_rl/control_train_multi-critic.sh
+++ scripts/reinforcement_learning/rsl_rl/control_train_multi-critic.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ALGORITHM="ppo-multi-critic"
 ROBOT="IntBall2"
-TASKS='[GoToPose3D, TrackVelocities3D, GoThroughPoses3D, GoToPosition3DWithObstacles]'
-for SEED in {1..5}
+for SEED in {2..5}
 do
-    ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train_control.py --task=Isaac-RANS-MultiTask-v0 --headless env.robot_name=$ROBOT env.tasks_names=$TASKS --algorithm=$ALGORITHM --seed=$SEED
+    ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train_control.py --task=Isaac-RANS-MultiTask-v0 --headless env.robot_name=$ROBOT env.tasks_names="[GoToPose3D, TrackVelocities3D, GoThroughPoses3D, GoToPosition3DWithObstacles]" --algorithm=$ALGORITHM --seed=$SEED
     sleep 5
+    echo "Done Training"
 done
