4265e02a4515b2ef2494c65fdb16d9253b332de3
ricard-inho <ricardmarsalicastan@gmail.com>
2026-01-08 18:27:06 +0100

    simple correct num envs

--- scripts/reinforcement_learning/rsl_rl/control_train_pcgrad_simple.sh
+++ scripts/reinforcement_learning/rsl_rl/control_train_pcgrad_simple.sh
@@ -3,7 +3,7 @@ ALGORITHM="ppo"
 ROBOT="IntBall2"
 for SEED in {1..5}
 do
-    ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train_control.py --task=Isaac-RANS-MultiTask-v0 --headless env.robot_name=$ROBOT env.tasks_names="[GoToPose3D, TrackVelocities3D, GoThroughPoses3D]" --algorithm=$ALGORITHM --seed=$SEED
+    ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train_control.py --task=Isaac-RANS-MultiTask-v0 --headless env.robot_name=$ROBOT env.tasks_names="[GoToPose3D, TrackVelocities3D, GoThroughPoses3D]" --algorithm=$ALGORITHM --seed=$SEED --num_envs=4095
     sleep 5
     echo "Done Training"
 done
