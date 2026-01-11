082057c84a072ecfb6d8c2c7ffe57e16aabbb492
ricard-inho <ricardmarsalicastan@gmail.com>
2026-01-11 22:21:12 +0100

    train simple semEmb

--- source/isaaclab_tasks/isaaclab_tasks/rans/environments/single_robot_multi_task/agents/rsl_rl_ppo-beta-memory_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/rans/environments/single_robot_multi_task/agents/rsl_rl_ppo-beta-memory_cfg.py
@@ -13,10 +13,10 @@ class SingleRobotMultiTaskPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 16
     max_iterations = 4000
     save_interval = 1000
-    experiment_name = "multitask_memory_control_beta_simple_taskid"
+    experiment_name = "multitask_memory_control_beta_simple_semEmb"
     logger = "wandb"
     wandb_kwargs = {
-        "project": "multitask_memory_control_beta_simple_taskid",
+        "project": "multitask_memory_control_beta_simple_semEmb",
         "entity": "spacer-rl",
         "group": "zeroG",
     }
