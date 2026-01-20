b5f1a743bae7b15d2c40e76ea39de541279aa8eb
ricard-inho <ricardmarsalicastan@gmail.com>
2026-01-20 15:15:08 +0100

    Hypernet PCGrad TaskID S

--- source/isaaclab_tasks/isaaclab_tasks/rans/environments/single_robot_multi_task/agents/rsl_rl_ppo-beta-memory_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/rans/environments/single_robot_multi_task/agents/rsl_rl_ppo-beta-memory_cfg.py
@@ -13,10 +13,10 @@ class SingleRobotMultiTaskPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     num_steps_per_env = 16
     max_iterations = 4000
     save_interval = 1000
-    experiment_name = "multitask_memory_control_beta_s_pcgrad_new_obstacles_semEmb"
+    experiment_name = "multitask_memory_control_beta_S_pcgrad_new_obstacles_taskID"
     logger = "wandb"
     wandb_kwargs = {
-        "project": "multitask_memory_control_beta_s_pcgrad_new_obstacles_semEmb",
+        "project": "multitask_memory_control_beta_S_pcgrad_new_obstacles_taskID",
         "entity": "spacer-rl",
         "group": "zeroG",
     }
